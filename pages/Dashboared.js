426b78fa58c6de069edad2aeb7e166a8244741dc
suhasmoshimoshi <suhas09.moshimoshi@gmail.com>
2023-02-02 15:29:17 +0530

    background color

--- pages/Dashboared.js
+++ pages/Dashboared.js
@@ -54,15 +54,17 @@ function Dashboared() {
         </div> */}
         <div className="grid grid-cols-5 gap-2   bg-red-50 pt-2 pr-5">
           <div>
-            <a class="block ml-4  max-w-sm min-w-[250px] p-6 bg-black border border-gray-200 rounded-lg shadow-md hover:bg-gray-100 hover:text-black text-white  ">
+            <a class="block ml-4  max-w-sm min-w-[250px] p-6 bg-white text-black border border-gray-200 rounded-lg shadow-md hover:bg-[#F1AC33] hover:text-white   ">
               <h5 class="mb-2 text-center  text-2xl font-bold tracking-tight  ">
                 JOB OPENINGS
               </h5>
-              <p class="font-normal text-xl text-center text-gray-700 ">3</p>
+              <p class="font-normal text-xl text-center text-gray-700 hover:text-white ">
+                3
+              </p>
             </a>
           </div>
           <div>
-            <a class="block ml-4  max-w-sm p-6 min-w-[250px] bg-black border border-gray-200 rounded-lg shadow-md hover:bg-gray-100 text-white hover:text-black">
+            <a class="block ml-4  max-w-sm p-6 min-w-[250px] bg-white border border-gray-200 rounded-lg shadow-md hover:bg-[#F1AC33] text-black hover:text-white">
               <h5 class="mb-2 text-center  text-2xl font-bold tracking-tight ">
                 CONTACT FORM
               </h5>
@@ -70,7 +72,7 @@ function Dashboared() {
             </a>
           </div>
           <div>
-            <a class="block ml-4  max-w-sm p-6 min-w-[250px] bg-black border border-gray-200 rounded-lg shadow-md hover:bg-gray-100 text-white hover:text-black">
+            <a class="block ml-4  max-w-sm p-6 min-w-[250px] bg-white border border-gray-200 rounded-lg shadow-md hover:bg-[#F1AC33] text-black hover:text-white">
               <h5 class="mb-2 text-center  text-2xl font-bold tracking-tight ">
                 TEAMS
               </h5>
@@ -78,7 +80,7 @@ function Dashboared() {
             </a>
           </div>
           <div>
-            <a class="block ml-4  max-w-sm p-6 min-w-[250px] bg-black border border-gray-200 rounded-lg shadow-md hover:bg-gray-100 text-white hover:text-black">
+            <a class="block ml-4  max-w-sm p-6 min-w-[250px] bg-white border border-gray-200 rounded-lg shadow-md hover:bg-[#F1AC33] text-black hover:text-white">
               <h5 class="mb-2 text-center  text-2xl font-bold tracking-tight ">
                 BLOGS
               </h5>
@@ -86,7 +88,7 @@ function Dashboared() {
             </a>
           </div>
           <div>
-            <a class="block ml-4  max-w-sm p-6 min-w-[250px] bg-black border border-gray-200 rounded-lg shadow-md hover:bg-gray-100 text-white hover:text-black">
+            <a class="block ml-4  max-w-sm p-6 min-w-[250px] bg-white border border-gray-200 rounded-lg shadow-md hover:bg-[#F1AC33] text-black hover:text-white">
               <h5 class="mb-2 text-center  text-2xl font-bold tracking-tight ">
                 LIFE AT 1k
               </h5>
@@ -94,7 +96,7 @@ function Dashboared() {
             </a>
           </div>
           <div>
-            <a class="block ml-4  max-w-sm p-6 min-w-[250px] bg-black border border-gray-200 rounded-lg shadow-md hover:bg-gray-100 text-white hover:text-black">
+            <a class="block ml-4  max-w-sm p-6 min-w-[250px] bg-white border border-gray-200 rounded-lg shadow-md hover:bg-[#F1AC33] text-black hover:text-white">
               <h5 className="mb-2 text-center  text-2xl font-bold tracking-tight capitalize">
                 FOUNDERS
               </h5>
@@ -102,7 +104,7 @@ function Dashboared() {
             </a>
           </div>
           <div>
-            <a class="block ml-4  max-w-sm p-6 min-w-[250px] bg-black border border-gray-200 rounded-lg shadow-md hover:bg-gray-100 text-white hover:text-black">
+            <a class="block ml-4  max-w-sm p-6 min-w-[250px] bg-white border border-gray-200 rounded-lg shadow-md hover:bg-[#F1AC33] text-black hover:text-white">
               <h5 className="mb-2 text-center  text-2xl font-bold tracking-tight capitalize">
                 SUBSCRIBE
               </h5>
@@ -110,7 +112,7 @@ function Dashboared() {
             </a>
           </div>
           <div>
-            <a class="block ml-4  max-w-sm p-6 min-w-[250px] bg-black border border-gray-200 rounded-lg shadow-md hover:bg-gray-100 text-white hover:text-black">
+            <a class="block ml-4  max-w-sm p-6 min-w-[250px] bg-white border border-gray-200 rounded-lg shadow-md hover:bg-[#F1AC33] text-black hover:text-white">
               <h5 className="mb-2 text-center  text-2xl font-bold tracking-tight capitalize">
                 1K NEWS ROOM
               </h5>
@@ -118,7 +120,7 @@ function Dashboared() {
             </a>
           </div>
           <div>
-            <a class="block ml-4  max-w-sm p-6 min-w-[250px] bg-black border border-gray-200 rounded-lg shadow-md hover:bg-gray-100 text-white hover:text-black">
+            <a class="block ml-4  max-w-sm p-6 min-w-[250px] bg-white border border-gray-200 rounded-lg shadow-md hover:bg-[#F1AC33] text-black hover:text-white">
               <h5 className="mb-2 text-center  text-2xl font-bold tracking-tight capitalize">
                 F . A . Q
               </h5>
@@ -126,7 +128,7 @@ function Dashboared() {
             </a>
           </div>
           <div>
-            <a class="block ml-4  max-w-sm p-6 min-w-[250px] bg-black border border-gray-200 rounded-lg shadow-md hover:bg-gray-100 text-white hover:text-black">
+            <a class="block ml-4  max-w-sm p-6 min-w-[250px] bg-white border border-gray-200 rounded-lg shadow-md hover:bg-[#F1AC33] text-black hover:text-white">
               <h5 className="mb-2 text-center  text-2xl font-bold tracking-tight capitalize">
                 BRANDS
               </h5>
