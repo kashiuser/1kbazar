ab669dc602498ced2720752836ca9d3b7833b7fa
selva19496 <selvakumar@ozzi.tech>
2023-02-15 17:06:24 +0530

    all pages is done

--- pages/HomeTestimonial.js
+++ pages/HomeTestimonial.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import React, { useEffect,useState } from "react";
 import Sidenav from "../components/sidebar/Sidenav";
 import { AgGridReact, AgGridColumn } from "ag-grid-react";
 import Grid from "@mui/material/Grid";
@@ -42,17 +42,41 @@ function HomeTestimonial() {
       console.log(err);
     }
   };
-  // const handelFormSubmit = async () => {
-  //   await fetch(url, { method: "POST", body: JSON.stringify(formData) })
-  //     .then((resp) => resp.json())
-  //     // .then((resp) => getCareerList())
-  //     .catch((err) => console.log(err));
-  // };
-  // console.log(formData);
 
   const [open, setOpen] = React.useState(false);
   const handleOpen = () => setOpen(true);
   const handleClose = () => setOpen(false);
+
+    /* update */
+    const [updateID, setUpdateId] = useState("");
+    const [show, setShow] = useState(false);
+
+    const [updateHomeTestimonial, setupdateHomeTestimonial] = useState({
+      title: "",
+      category: "",
+      content: "",
+      image: "",
+    });
+    const [updateHomeTestimonialErrors, setupdateHomeTestimonialErrors] = useState({
+      title: "",
+      category: "",
+      image: "",
+      content: "",
+    });
+  const handleUpdate = (ele) => {
+    setShow(true);
+    setupdateHomeTestimonial({
+      ...updateHomeTestimonial,
+      title: ele.title,
+      category: ele.category,
+      image: "",
+      content: ele.content,
+    });
+    setShow(true);
+    setUpdateId(ele);
+  };
+
+ 
 
   const columnDefs = [
     { headerName: "ID", field: "_id" },
@@ -83,6 +107,46 @@ function HomeTestimonial() {
       editable: true,
       sortable: true,
       filter: true,
+    },
+    {
+      headerName: "Update",
+      filter: false,
+      cellRendererFramework: (params) => (
+        <center onClick={() => handleUpdate(params.data)}>
+          <button type="button" class="btn btn-outline-primary">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="16"
+              height="16"
+              fill="currentColor"
+              class="bi bi-pencil"
+              viewBox="0 0 16 16"
+            >
+              <path d="M12.146.146a.5.5 0 0 1 .708 0l3 3a.5.5 0 0 1 0 .708l-10 10a.5.5 0 0 1-.168.11l-5 2a.5.5 0 0 1-.65-.65l2-5a.5.5 0 0 1 .11-.168l10-10zM11.207 2.5 13.5 4.793 14.793 3.5 12.5 1.207 11.207 2.5zm1.586 3L10.5 3.207 4 9.707V10h.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.5h.293l6.5-6.5zm-9.761 5.175-.106.106-1.528 3.821 3.821-1.528.106-.106A.5.5 0 0 1 5 12.5V12h-.5a.5.5 0 0 1-.5-.5V11h-.5a.5.5 0 0 1-.468-.325z" />
+            </svg>
+          </button>
+        </center>
+      ),
+    },
+    {
+      headerName: "Delete",
+      filter: false,
+      cellRendererFramework: (params) => (
+        <center onClick={() => handleDelete(params.data)}>
+          <button type="button" class="btn btn-outline-danger">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="16"
+              height="16"
+              fill="currentColor"
+              class="bi bi-trash-fill"
+              viewBox="0 0 16 16"
+            >
+              <path d="M2.5 1a1 1 0 0 0-1 1v1a1 1 0 0 0 1 1H3v9a2 2 0 0 0 2 2h6a2 2 0 0 0 2-2V4h.5a1 1 0 0 0 1-1V2a1 1 0 0 0-1-1H10a1 1 0 0 0-1-1H7a1 1 0 0 0-1 1H2.5zm3 4a.5.5 0 0 1 .5.5v7a.5.5 0 0 1-1 0v-7a.5.5 0 0 1 .5-.5zM8 5a.5.5 0 0 1 .5.5v7a.5.5 0 0 1-1 0v-7A.5.5 0 0 1 8 5zm3 .5v7a.5.5 0 0 1-1 0v-7a.5.5 0 0 1 1 0z" />
+            </svg>
+          </button>
+        </center>
+      ),
     },
     
   ];
